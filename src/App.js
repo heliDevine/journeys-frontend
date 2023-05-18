ba76777286fc2d9335ed90c6f69cb05762f46fef
Heli Devine <heli.devine@gmail.com>
2023-05-18 13:41:36 +0100

    added initial Stations component and react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import Journeys from './components/Journeys';
 import NavBar from './components/NavBar';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Stations from './components/Stations';
 
 const App = () => {
   return (
-    <div className="App">
-      <NavBar />
-      <div className="container" />
-      <h1>Journeys App</h1>
-
-      <Journeys />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div className="container" />
+        <NavBar />
+        <Routes>
+          <Route path="journeys" element={<Journeys />} />
+          <Route path="stations" element={<Stations />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
