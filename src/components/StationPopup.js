4dd7ad70f0e92f40241cd5eff1ae6ae403f7f9a7
Heli Devine <heli.devine@gmail.com>
2023-05-21 19:18:07 +0100

    Station components working now

--- src/components/StationPopup.js
+++ src/components/StationPopup.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
-export const StationPopup = ({ selectedStaion, onClose }) => {
-  const totalDistance = selectedStaion.totalJourneyDistanceFromStation;
+export const StationPopup = ({ selectedStation, onClose }) => {
+  const totalDistance = selectedStation.totalJourneyDistanceFromStation;
+  const displayDistance = isNaN(totalDistance)
+    ? 'No total distance calculated'
+    : `${(totalDistance / 1000).toFixed(1)} kilometers`;
+  const emissionsSaved = isNaN(totalDistance)
+    ? ''
+    : `${((totalDistance / 1000) * 107.5).toFixed(0)} gCO2`;
 
-  if (totalDistance <= 0) {
-    return 'no journeys done';
-  } else
-    return (
-      <div className="popup">
-        <div className="popup-content">
-          <h2>GREEN HELSINKI</h2>
-          <p>{(totalDistance / 1000).toFixed(1)} kilometers</p>
-          <p>
-            CO2 emissions saved {((totalDistance / 1000) * 107.5).toFixed(0)}
-            gCO2
-          </p>
-          <button className="close-button" onClick={onClose}>
-            Close
-          </button>
-        </div>
+  return (
+    <div className="popup">
+      <div className="popup-content">
+        <h2>GREEN HELSINKI</h2>
+        <p>{displayDistance}</p>
+        <p>CO2 emissions saved {emissionsSaved}</p>
+        <button className="close-button" onClick={onClose}>
+          Close
+        </button>
       </div>
-    );
+    </div>
+  );
 };
