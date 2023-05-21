028fe05fd610b6cba6fd0e91d9706aeb35907cdf
Heli Devine <heli.devine@gmail.com>
2023-05-21 18:36:13 +0100

    added Stations component and checked it fetches data from the stations endpoint

--- src/components/Stations.js
+++ src/components/Stations.js
@@ -1,11 +1,59 @@
 import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import StationList from './StationList';
 
-function Stations() {
+const Stations = () => {
+  const [stations, setStations] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [lastPage, setLastPage] = useState(0);
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios
+      .get(`http://localhost:8080/stations/?pageNo=${page}`)
+      .then(res => {
+        setStations(prevState => [...res.data.content]);
+        setIsLoading(false);
+        setLastPage(res.data.totalPages);
+        console.log(res.data);
+        return res.data;
+      })
+      .catch(err => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  }, [page]);
+
   return (
-    <div>
-      <h1>Stations</h1>
+    <div className="station_component">
+      {stations && <StationList stations={stations} />}
+      <div className="pagination">
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <button
+              className="pagination_button"
+              disabled={page === 0}
+              onClick={() => setPage(prevState => prevState - 1)}
+            >
+              Prev
+            </button>
+            <p className="page_number">{page + 1}</p>
+            <button
+              className="pagination_button"
+              disabled={page === lastPage - 1}
+              onClick={() => setPage(prevState => prevState + 1)}
+            >
+              Next
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default Stations;
